af0505938f8399991863e76afb61fa1bfc925f4f
Vu <nguyentuanvu9x@gmail.com>
2023-08-21 10:43:47 +0300

    Adding: Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,10 +2,14 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int result = 1;
+        for(int i = 0; i < power; i++) {
+            result *= numberToPrint;
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-        new Power().printPower(10, 3);
+        new Power().printPower(0, 4);
     }
 }
