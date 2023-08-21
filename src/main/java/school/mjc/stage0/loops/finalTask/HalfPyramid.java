b4fe56b2d366d48d848b39ae58a9185c5a074b92
Vu <nguyentuanvu9x@gmail.com>
2023-08-21 10:47:18 +0300

    Adding: HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for(int heitght = 1; heitght <= cathetusLength; heitght++) {
+            for(int leftSide = cathetusLength; leftSide > 0; leftSide--) {
+                if(leftSide > heitght) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
