e27742cfd2d5920884b74660a9691fcc1eea925e
Vu <nguyentuanvu9x@gmail.com>
2023-08-21 10:37:42 +0300

    Adding: Pyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for(int heitght = 1; heitght <= cathetusLength; heitght++) {
+            for(int leftSide = cathetusLength; leftSide > 0; leftSide--) {
+                if(leftSide > heitght) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(leftSide);
+                }
+            }
+
+            for(int rightSide = 2; rightSide <= cathetusLength; rightSide++) {
+                if(rightSide <= heitght) {
+                    System.out.print(rightSide);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println("");
+        }
 
     }
 
