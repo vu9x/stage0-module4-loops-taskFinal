e65828b636cd19e5f394aabfb9435a361b6db20d
Vu <nguyentuanvu9x@gmail.com>
2023-08-21 10:26:13 +0300

    Adding: MultiplicationTable.java

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public static void printTable(int numberTableToPrint){
+        for(int i = 1; i <= 10; i++) {
+            int result = i * numberTableToPrint;
+
+            System.out.println(i + " x " + numberTableToPrint + " = " + result);
+        }
+    }
+
+    public static void main(String[] args) {
+        printTable(0);
     }
 }
